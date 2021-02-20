8891ee26807dbed7beadc2fcbda48c20c29d131d
Agnomilted <72987041+Agnomilted@users.noreply.github.com>
2021-02-20 20:56:20 +0300

    First upload

---
+++ tinywm.c
@@ -0,0 +1,53 @@
+/* TinyWM is written by Nick Welch <nick@incise.org> in 2005 & 2011.
+ *
+ * This software is in the public domain
+ * and is provided AS IS, with NO WARRANTY. */
+
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define superkey Mod4Mask
+
+int main(void)
+{
+    Display * dpy;
+    XWindowAttributes attr;
+    XButtonEvent start;
+    XEvent ev;
+
+    if(!(dpy = XOpenDisplay(0x0))) return 1;
+
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XK_Return), superkey|ShiftMask,
+            DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+    XGrabButton(dpy, 1, superkey, DefaultRootWindow(dpy), True,
+            ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+    XGrabButton(dpy, 3, superkey, DefaultRootWindow(dpy), True,
+            ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+
+    start.subwindow = None;
+    for(;;)
+    {
+        XNextEvent(dpy, &ev);
+        if(ev.type == KeyPress && ev.xkey.subwindow != None)
+            XRaiseWindow(dpy, ev.xkey.subwindow);
+        else if(ev.type == ButtonPress && ev.xbutton.subwindow != None)
+        {
+            XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
+            start = ev.xbutton;
+        }
+        else if(ev.type == MotionNotify && start.subwindow != None)
+        {
+            int xdiff = ev.xbutton.x_root - start.x_root;
+            int ydiff = ev.xbutton.y_root - start.y_root;
+            XMoveResizeWindow(dpy, start.subwindow,
+                attr.x + (start.button==1 ? xdiff : 0),
+                attr.y + (start.button==1 ? ydiff : 0),
+                MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
+                MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
+        }
+        else if(ev.type == ButtonRelease)
+            start.subwindow = None;
+    }
+}
+
